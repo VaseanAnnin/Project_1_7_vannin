ea95ae5ba8cbfa3ffc0d5befe4d901d34c2bafd9
Vasean Annin <vaseanannin@gmail.com>
2026-06-24 21:40:06 -0400

    Added triple quote block for assignment requirements

---
+++ MusicTracker.py
@@ -0,0 +1,12 @@
+"""
+Program: Music Collection and Practice Tracker
+
+Author: Vasean Annin
+
+Purpose: This aplication will store and manage the users
+music collection as well as track their progress as they practice
+the songs in the collection
+
+Date: 06/24/2026
+
+"""
